94a99e762f1bf5d34ac1676239e9b7788902f74e
Fabrizio Marconi <fabrizio.marconi@jef.it>
2021-10-13 16:15:51 +0200

    better fix take out reminders

--- src/app/modules/main/components/pages/home/home.component.ts
+++ src/app/modules/main/components/pages/home/home.component.ts
@@ -43,7 +43,7 @@ export class HomeComponent implements OnInit {
     ]).subscribe(
       ([volumeMessage, appointmentsMessage, list]) => this.list = [
         ...list
-        .filter(one => ['MEDICATION', 'MEAL', 'ACTIVITY'].includes(one.name))
+        .filter(one => !['MEDICATION', 'MEAL', 'ACTIVITY'].includes(one.name))
         .map(
           one => ({
             ...one,
