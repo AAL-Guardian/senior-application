48d9e20549f7bf11d4a8b4a94b5b2c1280cd9d9c
Fabrizio Marconi <fabrizio.marconi@jef.it>
2022-09-01 12:39:30 +0200

    added check for snooze

--- src/app/models/report-request.model.ts
+++ src/app/models/report-request.model.ts
@@ -14,7 +14,10 @@ export interface ReportRequest {
   date_updated: string,
   date_deleted?: string,
   time?: number
-
+  remind_again_times: number,
+  remind_again_minutes: number,
+  remind_later: number,
+  reminder_shown_times: number,
   description: string;
 
   report_question: ReportQuestion[];
